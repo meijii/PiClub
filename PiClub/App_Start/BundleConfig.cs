c78d6c8776282aba6c915df9daffdce66d345d6c
meijii <heyoones@gmail.com>
2017-03-26 00:02:31 +1300

    Adding custom social media buttons to login

--- PiClub/App_Start/BundleConfig.cs
+++ PiClub/App_Start/BundleConfig.cs
@@ -26,6 +26,11 @@ namespace PiClub
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/font-awesome.css",
+                "~/Content/site..css"));
         }
     }
 }
