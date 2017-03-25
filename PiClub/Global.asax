0caefed186a28b1b1c7d8e2b57bc1bb1fc50090c
meijii <heyoones@gmail.com>
2017-03-25 20:56:56 +1300

    Add project files.

---
+++ PiClub/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PiClub.MvcApplication" Language="C#" %>
---
+++ PiClub/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PiClub.Startup))]
+namespace PiClub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
